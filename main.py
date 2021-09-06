13cedcb33fa40d6cde91d65cc9cca65ef5195d28
Cuong <Cuongtq88>
2021-09-06 10:13:45 +0700

    Update data

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import smtplib
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = '8BYkE123fBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sim.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
